359fda123d77c7e78b13858cd44171af6cce0f0d
Danila Petrov <daniilpetrov776@gmail.com>
2024-04-20 09:23:15 +0300

    Работающее выпадающее меню

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,17 @@
 document.querySelector('.main-header').classList.remove('main-header--no-js');
 
+let menu = document.querySelector('.main-header__list-wrapper');
 
+let menuButton = document.querySelector('.main-header__toggle');
+
+let menuButtonContent = document.querySelector('.main-header__toggle-lines')
+
+console.log(menuButtonContent);
+
+menu.classList.add('menu-closed');
+
+menuButton.onclick = function () {
+  menu.classList.toggle('menu-closed')
+  menu.classList.toggle('menu-opened')
+  menuButtonContent.classList.toggle('main-header__toggle-lines--x')
+}
