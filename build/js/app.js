faa2a69400aa33c6f4b488cfca6c0ca6851aca07
Danila Petrov <daniilpetrov776@gmail.com>
2024-05-10 19:39:39 +0300

    Починил js, поправил отвалившуюся картинку

--- build/js/app.js
+++ build/js/app.js
@@ -1,18 +1,12 @@
 document.querySelector('.main-header').classList.remove('main-header--no-js');
 
 let menu = document.querySelector('.main-header__list-wrapper');
-
 let menuButton = document.querySelector('.main-header__toggle');
-
 let menuButtonContent = document.querySelector('.main-header__toggle-lines')
-
-
 
 console.log(menuButtonContent);
 
 const mobileMediaQuery = window.matchMedia('(max-width: 768px)')
-
-
   menu.classList.add("main-header__list-wrapper--no-animation");
     setTimeout(() => {
       menu.classList.remove("main-header__list-wrapper--no-animation");
@@ -30,12 +24,12 @@ mobileMediaQuery.addEventListener("change", function (event) {
 });
 
 menu.classList.add('main-header__list-wrapper--menu-close');
-
-menuButton.onclick = function () {
+if (menuButton) {menuButton.addEventListener ("click", function () {
   menu.classList.toggle('main-header__list-wrapper--menu-close');
   menu.classList.toggle('main-header__list-wrapper--menu-open');
   menuButtonContent.classList.toggle('main-header__toggle-lines--x');
-}
+})}
+
 let videoPlayer = document.querySelector('.video__player');
 let videoLink = document.querySelector('.video__play-button');
 let videoPreview = document.querySelector('.video__video-wrapper');
@@ -44,10 +38,14 @@ console.log(videoLink);
 console.log(videoPlayer);
 console.log(videoPreview);
 
-videoLink.addEventListener("click",
+if (videoLink) {
+  videoLink.addEventListener("click",
 function (event) {
   event.preventDefault();
   videoPlayer.classList.toggle('video__player--no-js');
   videoPreview.classList.toggle('video__video--hidden');
   videoLink.classList.toggle('video__play-button--hidden');
 });
+}
+
+
